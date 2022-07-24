6cbdbb65ef267acb4f16be736657cfce58118f98
Mitch Polifka <mjpolifka@gmail.com>
2022-07-24 17:09:22 -0400

    cleaning up

--- app.py
+++ app.py
@@ -1,21 +1,3 @@
 from satisfactory_tools import create_app
 
 create_app()
-
-
-"""
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-from app.models import Ingredient
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-"""
--- satisfactory_tools/__init__.py
+++ satisfactory_tools/__init__.py
@@ -3,18 +3,14 @@ from satisfactory_tools.db import db
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    
     app.config.from_mapping(SQLALCHEMY_DATABASE_URI=f'sqlite:///database.db',
         SQLALCHEMY_TRACK_MODIFICATIONS=False)
-    db.init_app(app)
     
-    @app.route('/hello/')
-    def hello():
-        return "Hello, world!"
+    db.init_app(app)
     
     from satisfactory_tools import components, init_db
     app.register_blueprint(components.bp)
     app.register_blueprint(init_db.bp)
-
-    app.add_url_rule("/", endpoint="index")
     
     return app

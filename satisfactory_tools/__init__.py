4347b8e8996c158c645eaf1039d83467fb39412a
Mitch Polifka <mjpolifka@gmail.com>
2022-07-24 17:07:07 -0400

    init_db works again, must navigate to /init_db to run

--- satisfactory_tools/__init__.py
+++ satisfactory_tools/__init__.py
@@ -5,13 +5,15 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SQLALCHEMY_DATABASE_URI=f'sqlite:///database.db',
         SQLALCHEMY_TRACK_MODIFICATIONS=False)
+    db.init_app(app)
     
     @app.route('/hello/')
     def hello():
         return "Hello, world!"
     
-    from satisfactory_tools import components
+    from satisfactory_tools import components, init_db
     app.register_blueprint(components.bp)
+    app.register_blueprint(init_db.bp)
 
     app.add_url_rule("/", endpoint="index")
     

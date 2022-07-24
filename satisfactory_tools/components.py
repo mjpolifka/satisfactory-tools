cc09f547c65f908bb69a0cd8d06db84faafa60c3
Mitch Polifka <mjpolifka@gmail.com>
2022-07-24 17:15:01 -0400

    displaying all components and their ingredients

--- satisfactory_tools/components.py
+++ satisfactory_tools/components.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template
+from satisfactory_tools.models import Ingredient
 
 bp = Blueprint("components", __name__)
 
 @bp.route("/")
 def index():
-    return render_template("components/index.html")
+    components = Ingredient.query.all()
+    return render_template("components/index.html", components=components)
 
 @bp.route("/test")
 def test():

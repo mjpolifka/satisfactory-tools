968defa5c1ba6ac2456e70f2b8c2a39d3064ecf0
Mitch Polifka <mjpolifka@gmail.com>
2022-07-25 15:42:05 -0400

    db updates, added readme

---
+++ readme.md
@@ -0,0 +1,43 @@
+
+# Satisfactory Tools
+
+## To run:
+set environment variables (examples are in windows cmd):
+
+Windows CMD:
+- set FLASK_ENV=development
+- set FLASK_APP=app.py
+Windows PowerShell:
+- $env:FLASK_ENV="development"
+- $env:FLASK_APP="app.py"
+Linux/Unix/(?Mac?):
+- TODO
+
+run `flask run` to start the development server
+in your browser, navigate to http://localhost:5000/init_db/ the first time you run it
+
+Then to access the app, navigate to http://localhost:5000/
+
+
+## "Documentation" (for now, basically just some notes)
+
+uses flask and flask-sqlalchemy, with an sqlite database
+doesn't do much yet
+
+
+## TODO List
+
+### In-Progress
+- Homepage (components/index.html | components.py/index) should group components by where they're made
+
+### Planning
+- in components/index.html make each item a link to /components/<id>
+    - create a new view for an individual component
+        - show the ingredients for the item
+        - eventually show the ingredients for all ingredients back to an item with a made_in in ('by_hand', 'miner')
+        - after that, be able to limit the output needed and recalculate all the ingredients
+- haven't figured out the 'by_hand' items yet
+- need to start calculating items per minute, I'm guessing it's a function shared by everything
+- could use some styling, probably bootstrap
+- is there a way to make this callable as an api without decoupling the templates?
+    - if not, start considering when is a good time to refactor this into frontend/backend
--- satisfactory_tools/init_db.py
+++ satisfactory_tools/init_db.py
@@ -10,21 +10,76 @@ def init_db():
     db.drop_all()
     db.create_all()
 
-    iron_ore = Ingredient(name='iron ore', made_in='miner', speed=60, quantity=60)
-    iron_ingot = Ingredient(name='iron ingot', made_in='smelter', speed=30, quantity=15)
-    iron_rod = Ingredient(name='iron rod', made_in='constructor', speed=30, quantity=15)
-    iron_plate = Ingredient(name='iron plate', made_in='constructor', speed=30, quantity=30)
-    screw = Ingredient(name='screw', made_in='constructor', speed=30, quantity=20)
-    reinforced_iron_plate = Ingredient(name='reinforced iron plate', made_in='assembler', speed=60, quantity=5)
+    list = []
 
+    # By Hand
+    made_in = 'by_hand'
+    # need to add leaves, wood, alien components, etc
+
+    # Miner
+    made_in = 'miner'
+    iron_ore = Ingredient(name='iron ore', made_in=made_in, speed=1, quantity=1)
+    copper_ore = Ingredient(name='copper ore', made_in=made_in, speed=1, quantity=1)
+    limestone = Ingredient(name='limestone', made_in=made_in, speed=1, quantity=1)
+    coal = Ingredient(name='coal', made_in=made_in, speed=1, quantity=1)
+    list.extend([iron_ore, copper_ore, limestone, coal])
+
+    # Smelter
+    made_in = 'smelter'
+    iron_ingot = Ingredient(name='iron ingot', made_in=made_in, speed=2, quantity=1)
+    copper_ingot = Ingredient(name='copper ingot', made_in=made_in, speed=2, quantity=1)
+    list.extend([iron_ingot, copper_ingot])
+
+    # Foundry
+    made_in = 'foundry'
+    steel_ingot = Ingredient(name='steel ingot', made_in=made_in, speed=4, quantity=3)
+    list.extend([steel_ingot])
+    
+    # Constructor
+    made_in = 'contructor'
+    iron_rod = Ingredient(name='iron rod', made_in=made_in, speed=4, quantity=1)
+    iron_plate = Ingredient(name='iron plate', made_in=made_in, speed=6, quantity=2)
+    screw = Ingredient(name='screw', made_in=made_in, speed=6, quantity=4)
+    wire = Ingredient(name='wire', made_in=made_in, speed=4, quantity=2)
+    cable = Ingredient(name='cable', made_in=made_in, speed=2, quantity=1)
+    concrete = Ingredient(name='concrete', made_in=made_in, speed=4, quantity=1)
+    copper_sheet = Ingredient(name='copper sheet', made_in=made_in, speed=6, quantity=1)
+    #biomass is made from ingredients harvested by hand
+    list.extend([iron_rod, iron_plate, wire, cable, concrete, copper_sheet])
+
+    # Assembler
+    made_in = 'assembler'
+    reinforced_iron_plate = Ingredient(name='reinforced iron plate', made_in=made_in, speed=12, quantity=1)
+    list.extend([reinforced_iron_plate])
+
+    # Manufacturer
+    made_in = 'manufacturer'
+    list.extend([])
+
+
+
+    # Smelter
     Recipe(component=iron_ingot, ingredient=iron_ore, quantity=1)
+    Recipe(component=copper_ingot, ingredient=copper_ore, quantity=1)
+
+    # Foundry
+    Recipe(component=steel_ingot, ingredient=iron_ore, quantity=3)
+    Recipe(component=steel_ingot, ingredient=coal, quantity=3)
+
+    # Constructor
     Recipe(component=iron_rod, ingredient=iron_ingot, quantity=1)
     Recipe(component=iron_plate, ingredient=iron_ingot, quantity=3)
     Recipe(component=screw, ingredient=iron_rod, quantity=1)
-    Recipe(component=reinforced_iron_plate, ingredient=iron_plate, quantity=1)
-    Recipe(component=reinforced_iron_plate, ingredient=screw, quantity=1)
+    Recipe(component=wire, ingredient=copper_ingot, quantity=1)
+    Recipe(component=cable, ingredient=wire, quantity=2)
+    Recipe(component=concrete, ingredient=limestone, quantity=3)
+    Recipe(component=copper_sheet, ingredient=copper_ingot, quantity=2)
 
-    db.session.add_all([iron_ore, iron_ingot, iron_rod, iron_plate, screw, reinforced_iron_plate])
+    # Assembler
+    Recipe(component=reinforced_iron_plate, ingredient=iron_plate, quantity=6)
+    Recipe(component=reinforced_iron_plate, ingredient=screw, quantity=12)
+
+    db.session.add_all(list)
 
     db.session.commit()
 

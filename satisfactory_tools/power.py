e93b4547afe47d48187513660d621c4a79cc9be4
Mitch Polifka <mjpolifka@gmail.com>
2022-08-13 11:55:27 -0400

    finished oil power calculator

--- satisfactory_tools/power.py
+++ satisfactory_tools/power.py
@@ -4,5 +4,12 @@ bp = Blueprint("power", __name__, url_prefix='/power')
 
 @bp.route("/")
 def power():
+    power_type = request.args.get("power_type")
+    oil_incoming = request.args.get("oil_incoming")
+
+    data = {
+        "power_type": power_type,
+        "oil_incoming": oil_incoming
+    }
     
-    return render_template('power.html')
+    return render_template('power.html', data=data)
